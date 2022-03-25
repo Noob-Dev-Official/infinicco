fa8e63819b5a270c02da553da41cf2ea54cde71a
Muntasir2001 <muntasirenamalhuda@gmail.com>
2022-03-25 12:01:00 +0000

    added ejs, removed htmls

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
 const express = require('express');
 
+const blogs = [
+   { title: 'hello', snippet: 'klasfklasasjkllka' },
+   { title: 'bye', snippet: 'klasfklasasjkllka' },
+   { title: 'hey', snippet: 'klasfklasasjkllka' },
+];
+
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
 // listen for requests
-app.listen(3000);
+app.listen(3000, 'localhost', () => {
+   console.log('Im upp!!');
+});
 
 app.get('/', (req, res) => {
-   // res.send('<p>homepage</p>');
-   res.sendFile('./views/index.html', { root: __dirname });
+   res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-   // res.send('<p>aboutpage</p>');
-   res.sendFile('./views/about.html', { root: __dirname });
+   res.render('about', { title: 'About' });
 });
 
-app.get('/about-us', (req, res) => {
-   res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+   res.render('create-blog', { title: 'Create Blog' });
 });
 
 // 404 page
 app.use((req, res) => {
-   res.status(404).sendFile('./views/404.html', { root: __dirname });
+   res.render('404', { title: '404' });
 });
