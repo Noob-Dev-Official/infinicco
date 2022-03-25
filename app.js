1f268bb35da2c6cf1c0a59b532a776fd7f1e61fb
Muntasir2001 <muntasirenamalhuda@gmail.com>
2022-03-25 09:22:45 +0000

    finished ep6

--- app.js
+++ app.js
@@ -7,5 +7,20 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res) => {
-   res.send();
+   // res.send('<p>homepage</p>');
+   res.sendFile('./views/index.html', { root: __dirname });
+});
+
+app.get('/about', (req, res) => {
+   // res.send('<p>aboutpage</p>');
+   res.sendFile('./views/about.html', { root: __dirname });
+});
+
+app.get('/about-us', (req, res) => {
+   res.redirect('/about');
+});
+
+// 404 page
+app.use((req, res) => {
+   res.status(404).sendFile('./views/404.html', { root: __dirname });
 });
