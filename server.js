84e4d563ead41d5d211eacb2ca18b444b8aedf2d
Muntasir <muntasirenamalhuda@gmail.com>
2022-03-26 11:53:46 +0000

    added css support

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
 const blogs = [
-   { title: 'hello', snippet: 'klasfklasasjkllka' },
-   { title: 'bye', snippet: 'klasfklasasjkllka' },
-   { title: 'hey', snippet: 'klasfklasasjkllka' },
+	{ title: 'hello', snippet: 'klasfklasasjkllka' },
+	{ title: 'bye', snippet: 'klasfklasasjkllka' },
+	{ title: 'hey', snippet: 'klasfklasasjkllka' },
 ];
 
 // express app
@@ -12,24 +12,26 @@ const app = express();
 // register view engine
 app.set('view engine', 'ejs');
 
+app.use(express.static(__dirname + '/public'));
+
 // listen for requests
 app.listen(3000, 'localhost', () => {
-   console.log('Im upp!!');
+	console.log('Im upp!!');
 });
 
 app.get('/', (req, res) => {
-   res.render('index', { title: 'Home', blogs });
+	res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-   res.render('about', { title: 'About' });
+	res.render('about', { title: 'About' });
 });
 
 app.get('/blogs/create', (req, res) => {
-   res.render('create-blog', { title: 'Create Blog' });
+	res.render('create-blog', { title: 'Create Blog' });
 });
 
 // 404 page
 app.use((req, res) => {
-   res.render('404', { title: '404' });
+	res.render('404', { title: '404' });
 });
